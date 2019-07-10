cd15d7809833faebe05b48ab76e8f649b59850cf
bhy <450215151@qq.com>
2019-07-10 16:32:09 +0800

    加入storybook-static

---
+++ storybook-static/main.d768c43ab5c127f9ad44.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{395:function(n,o,p){p(396),p(499),n.exports=p(894)},499:function(n,o,p){"use strict";p.r(o);p(500),p(639),p(648),p(847),p(850),p(889)}},[[395,1,2]]]);
